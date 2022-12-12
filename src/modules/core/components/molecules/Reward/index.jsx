d5d8e76d672bdc28320df8ae1e0e061b97595999
Ayda Yineth Montoya Henao <aidamontoya96@gmail.com>
2022-12-12 14:15:48 -0500

    Se da diseño al componente Reward PM 131

--- src/modules/core/components/molecules/Reward/index.jsx
+++ src/modules/core/components/molecules/Reward/index.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 export default function Reward() {
   return (
     <div className="mvl-reward-content">
-      <div>
+      <div className="mvl-reward-content-image">
         <img src="https://i.postimg.cc/7PKWqdrZ/marvel-insider-master.jpg" alt="" />
       </div>
-      <div>
-        <h6>MARVEL INSIDER</h6>
+      <div className="mvl-reward-content-text">
+        <h6 className="mvl-reward-title">MARVEL INSIDER</h6>
         <h4>Whatch, Earn, Redeem!</h4>
         <p>Get rewarded for doing what you already do as a fan.</p>
+        <button className="mvl-reward button-red">JOIN NOW</button>
+        <p className="mvl-reward-conditions">Terms and Conditions Apply</p>
       </div>
     </div>
   );
