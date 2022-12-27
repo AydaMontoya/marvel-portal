dce73fedaa2fe05372e5ad9fe7e6ac5fb671bf54
Ayda Yineth Montoya Henao <aidamontoya96@gmail.com>
2022-12-26 19:34:52 -0500

    Llamado a la API de la PM 253

--- src/modules/core/components/molecules/CharacterSpotlight/index.jsx
+++ src/modules/core/components/molecules/CharacterSpotlight/index.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.scss';
+import { getComics } from 'src/modules/core/services/comics.api-rest';
+import Loading from '../../atoms/Loading';
+import { EmptyState } from 'src/modules/marvel-characters/components/CharacterGrid';
+import PropTypes from 'prop-types';
+
+const ITEM_PER_PAGE = 4;
 
 export default function CharacterSpotlight() {
+  const [comics, setComics] = useState([]);
+  useEffect(() => {
+    fetchComics();
+  }, []);
+  async function fetchComics() {
+    const data = await getComics();
+    setComics(data.results);
+    console.log(data);
+  }
+
   return (
     <section>
       <div className="container-principal-comics">
@@ -9,21 +25,7 @@ export default function CharacterSpotlight() {
           <h1>CHARACTERS SPOTLIGHT</h1>
         </div>
         <div className="container-grid-comics">
-          <div>
-            <div>
-              <img
-                className="image-comics"
-                src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/shuri_bp_card_image.jpg"
-                alt=""
-              />
-            </div>
-            <div>
-              <p className="container-text-comics">COMICS</p>
-              <a className="link-comics" href="#">
-                Aqui va el titulo
-              </a>
-            </div>
-          </div>
+          <ComicsGrid comics={comics} isLoading={Loading} itemsPerPage={ITEM_PER_PAGE} />
           <div>
             <div>
               <img
@@ -74,3 +76,40 @@ export default function CharacterSpotlight() {
     </section>
   );
 }
+
+ComicsGrid.propTypes = {
+  comics: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool,
+  itemsPerPage: PropTypes.number
+};
+
+function ComicsGrid({ comics, isLoading, itemsPerPage }) {
+  if (isLoading && comics.length === 0) {
+    return <Loading amount={itemsPerPage} />;
+  }
+
+  if (comics.length === 0) {
+    return <EmptyState />;
+  }
+
+  return comics.map(({ title, image }, index) => <Comic title={title} image={image} key={index} />);
+}
+
+/* Comic.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string
+}; */
+
+const Comic = ({ image, title }) => {
+  <div>
+    <div>
+      <img className="image-comics" src={image} alt="" />
+    </div>
+    <div>
+      <p className="container-text-comics">COMICS</p>
+      <a className="link-comics" href="#">
+        {title}
+      </a>
+    </div>
+  </div>;
+};
